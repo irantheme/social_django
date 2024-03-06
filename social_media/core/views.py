0d14ee7aa36ec836560326bab8317a6c1cd772a7
iranianclever <iranianclever@gmail.com>
2024-03-06 21:17:35 +0330

    working on signup user

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 
 # Create your views here.
@@ -25,9 +26,17 @@ def signup(request):
                 messages.info(request,"Username Is Taken")
                 return redirect("signup")
             else:
-                user = User.objects.creat_user(username=username
+                user = User.objects.create_user(username=username
                     , email=email, password=password)
                 user.save()
+
+                # Log user in and redirect to settings page
+
+                # Create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
         else:
             messages.info(request,"Password Not Matching")
             return redirect("signup")
