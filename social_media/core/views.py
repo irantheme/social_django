c5e5d720e6ce922475a87f912b5a5d51ed23751f
iranianclever <iranianclever@gmail.com>
2024-03-09 20:01:33 +0330

    working on settings account

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 
 
 # Create your views here.
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'setting.html')
 
 def signup(request):
 
@@ -31,12 +37,14 @@ def signup(request):
                 user.save()
 
                 # Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
 
                 # Create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request,"Password Not Matching")
             return redirect("signup")
@@ -59,3 +67,8 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
