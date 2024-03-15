9ec8859baf814b8ad26b5c24bdf7ebfa6b47a00a
iranianclever <iranianclever@gmail.com>
2024-03-15 13:43:06 +0330

    working on Profile image

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -11,7 +11,14 @@ from .models import Profile
 
 @login_required(login_url="signin")
 def index(request):
-    return render(request, "index.html")
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, "index.html", {'user_profile': user_profile})
+
+
+@login_required(login_url="signin")
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
 
 
 @login_required(login_url="signin")
@@ -45,6 +52,7 @@ def settings(request):
 
 
 def signup(request):
+
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -60,21 +68,20 @@ def signup(request):
                 return redirect("signup")
             else:
                 user = User.objects.create_user(
-                    username=username, email=email, password=password
-                )
+                    username=username, email=email, password=password)
                 user.save()
 
                 # Log user in and redirect to settings page
-                user_login = auth.authenticate(username=username, password=password)
+                user_login = auth.authenticate(
+                    username=username, password=password)
                 auth.login(request, user_login)
 
                 # Create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(
-                    user=user_model, id_user=user_model.id
-                )
+                    user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect("settings")
+                return redirect('settings')
         else:
             messages.info(request, "Password Not Matching")
             return redirect("signup")
@@ -83,23 +90,23 @@ def signup(request):
 
 
 def signin(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
         user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
-            return redirect("/")
+            return redirect('/')
         else:
-            messages.info(request, "Credentials Invalid")
-            return redirect("signin")
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
     else:
-        return render(request, "signin.html")
+        return render(request, 'signin.html')
 
 
-@login_required(login_url="signin")
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
-    return redirect("signin")
+    return redirect('signin')
