245a5adf77504bf4684222783a778d88aa334c2c
iranianclever <iranianclever@gmail.com>
2024-03-15 12:13:55 +0330

    improve details somethings

--- social_media/core/views.py
+++ social_media/core/views.py
@@ -8,42 +8,43 @@ from .models import Profile
 
 # Create your views here.
 
-@login_required(login_url='signin')
-def index(request):
-    return render(request, 'index.html')
 
-@login_required(login_url='signin')
+@login_required(login_url="signin")
+def index(request):
+    return render(request, "index.html")
+
+
+@login_required(login_url="signin")
 def settings(request):
     user_profile = Profile.objects.get(user=request.user)
 
-    if request.method == 'POST':
-        if request.FILES.get('image') == None:
+    if request.method == "POST":
+        if request.FILES.get("image") == None:
             image = user_profile.profileimg
-            bio = request.POST['bio']
-            location = request.POST['location']
+            bio = request.POST["bio"]
+            location = request.POST["location"]
 
             user_profile.profileimg = image
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
 
-        if request.FILES.get('image') != None:
-            image = request.FILES.get('image')
-            bio = request.POST['bio']
-            location = request.POST['location']
+        if request.FILES.get("image") != None:
+            image = request.FILES.get("image")
+            bio = request.POST["bio"]
+            location = request.POST["location"]
 
             user_profile.profileimg = image
             user_profile.bio = bio
             user_profile.location = location
             user_profile.save()
-            
-        return redirect('settings')
 
+        return redirect("settings")
 
-    return render(request, 'setting.html', {'user_profile': user_profile})
+    return render(request, "setting.html", {"user_profile": user_profile})
+
 
 def signup(request):
-
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
@@ -52,14 +53,15 @@ def signup(request):
 
         if password == password2:
             if User.objects.filter(email=email).exists():
-                messages.info(request,"Email Taken")
+                messages.info(request, "Email Taken")
                 return redirect("signup")
             elif User.objects.filter(username=username).exists():
-                messages.info(request,"Username Is Taken")
+                messages.info(request, "Username Is Taken")
                 return redirect("signup")
             else:
-                user = User.objects.create_user(username=username
-                    , email=email, password=password)
+                user = User.objects.create_user(
+                    username=username, email=email, password=password
+                )
                 user.save()
 
                 # Log user in and redirect to settings page
@@ -68,33 +70,36 @@ def signup(request):
 
                 # Create a profile object for the new user
                 user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile = Profile.objects.create(
+                    user=user_model, id_user=user_model.id
+                )
                 new_profile.save()
-                return redirect('settings')
+                return redirect("settings")
         else:
-            messages.info(request,"Password Not Matching")
+            messages.info(request, "Password Not Matching")
             return redirect("signup")
     else:
-        return render(request,"signup.html")
+        return render(request, "signup.html")
 
 
 def signin(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         user = auth.authenticate(username=username, password=password)
 
         if user is not None:
             auth.login(request, user)
-            return redirect('/')
+            return redirect("/")
         else:
-            messages.info(request, 'Credentials Invalid')
-            return redirect('signin')
+            messages.info(request, "Credentials Invalid")
+            return redirect("signin")
     else:
-        return render(request, 'signin.html')
+        return render(request, "signin.html")
 
-@login_required(login_url='signin')
+
+@login_required(login_url="signin")
 def logout(request):
     auth.logout(request)
-    return redirect('signin')
+    return redirect("signin")
