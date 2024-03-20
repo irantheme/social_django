10a6c3c538100faacfb84e02e4e0529e217fe2ab
iranianclever <iranianclever@gmail.com>
2024-03-20 17:57:19 +0330

    working on delete post

--- social_media/core/urls.py
+++ social_media/core/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("search", views.search, name="search"),
     path("profile/<str:pk>", views.profile, name="profile"),
     path("like-post", views.like_post, name="like-post"),
+    path("delete", views.delete, name="delete"),
     path("signup", views.signup, name="signup"),
     path("signin", views.signin, name="signin"),
     path("logout", views.logout, name="logout"),
--- social_media/core/views.py
+++ social_media/core/views.py
@@ -118,6 +118,30 @@ def like_post(request):
 
 
 @login_required(login_url="signin")
+def delete(request):
+    pass
+    # username = request.user.username
+    # post_id = request.GET.get('post_id')
+
+    # post = Post.objects.get(id=post_id)
+
+    # like_filter = LikePost.objects.filter(
+    #     post_id=post_id, username=username).first()
+
+    # if like_filter == None:
+    #     new_like = LikePost.objects.create(post_id=post_id, username=username)
+    #     new_like.save()
+    #     post.no_of_likes = post.no_of_likes + 1
+    #     post.save()
+    #     return redirect('/')
+    # else:
+    #     like_filter.delete()
+    #     post.no_of_likes = post.no_of_likes - 1
+    #     post.save()
+    #     return redirect('/')
+
+
+@login_required(login_url="signin")
 def profile(request, pk):
     user_object = User.objects.get(username=pk)
     user_profile = Profile.objects.get(user=user_object)
