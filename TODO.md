5fcf3858977e3c274997ce1476325be9d83c4a92
iranianclever <iranianclever@gmail.com>
2024-03-22 10:57:01 +0330

    create delete action for posts

--- TODO.md
+++ TODO.md
@@ -1,4 +1,4 @@
 # TODO LIST
-- [ ] Create delete action for posts
+- [x] Create delete action for posts
 - [ ] Add dislike to posts
 - [ ] Add edit post to posts
--- social_media/core/views.py
+++ social_media/core/views.py
@@ -119,26 +119,22 @@ def like_post(request):
 
 @login_required(login_url="signin")
 def delete(request):
-    pass
-    # username = request.user.username
-    # post_id = request.GET.get('post_id')
+    username = request.user.username
+    post_id = request.POST.get('post_id')
 
     # post = Post.objects.get(id=post_id)
 
-    # like_filter = LikePost.objects.filter(
-    #     post_id=post_id, username=username).first()
+    post_filter = Post.objects.filter(
+        id=post_id, user=username).first()
 
-    # if like_filter == None:
-    #     new_like = LikePost.objects.create(post_id=post_id, username=username)
-    #     new_like.save()
-    #     post.no_of_likes = post.no_of_likes + 1
-    #     post.save()
-    #     return redirect('/')
-    # else:
-    #     like_filter.delete()
-    #     post.no_of_likes = post.no_of_likes - 1
-    #     post.save()
-    #     return redirect('/')
+    if post_filter == None:
+        return redirect('/')
+    else:
+        pass
+        post_filter.delete()
+        # post.no_of_likes = post.no_of_likes - 1
+        # post.save()
+        return redirect('/')
 
 
 @login_required(login_url="signin")
